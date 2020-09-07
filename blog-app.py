8da5d6ea2b25846f272590108bac920edd717ca6
shivaadith <shivaadith@gmail.com>
2020-09-07 17:22:48 +0530

    Add Contact page and re-organise styles-sheets/css code

--- blog-app.py
+++ blog-app.py
@@ -96,5 +96,10 @@ def new_posts():
         return render_template('new_post.html')
 
 
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
