28d075502474aae3a45c870a281347ab6e0c3f1f
shivaadith <shivaadith@gmail.com>
2020-10-04 18:51:07 +0530

    Add error logging to email.

--- config.py
+++ config.py
@@ -4,6 +4,24 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
+# FLASK_APP = environ.get('FLASK_APP')
+# SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SECRET_KEY = environ.get('SECRET_KEY')
+#
+# # Email server
+# MAIL_SERVER = "smtp.gmail.com"
+# MAIL_PORT = 587
+# # MAIL_USE_TLS = True
+# MAIL_USE_SSL = True
+# MAIL_USERNAME = environ.get('MAIL_USERNAME')
+# MAIL_DEFAULT_SENDER = environ.get('MAIL_DEFAULT_SENDER')
+# MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+#
+# # Admin list
+# ADMINS = environ.get('ADMINS')
+# # python -m smtpd -n -c DebuggingServer localhost:8025
+
 
 class Config:
     """Set Flask config variables. """
@@ -14,9 +32,9 @@ class Config:
 
     # Email server
     MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    # MAIL_USE_SSL = False
     MAIL_USERNAME = environ.get('MAIL_USERNAME')
     MAIL_DEFAULT_SENDER = environ.get('MAIL_DEFAULT_SENDER')
     MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
